967f7c556a2e9ab63bc6f4533d147549258b966a
Umer Noor <umer.noor@humber.ca>
2025-02-16 20:46:03 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -46,31 +46,58 @@ var ptx_lunr_docs = [
   "body": " A bare-bones Quaternion class   struct Quaternion { float w; Vec3 ijk; \/\/\/ These are the ijk components of the Quaternion };   "
 },
 {
-  "id": "ch-vectors",
+  "id": "sec-vector-code-start",
   "level": "1",
-  "url": "ch-vectors.html",
-  "type": "Chapter",
-  "number": "3",
-  "title": "Vectors",
-  "body": " Vectors   Vectors are the bedrock of 3D graphics, but it is easy to mix up positions (x, y, z) and directions (also x, y, z). We'll use 4D vectors to help clear up this mix-up. Don't freak out, they are super easy to use. The extra number (called w), will be a one for positions or a zero for directions. Don't go down the rabbit hole of imagining the 4th dimension, there are only dragons there. Just think of them for now as a flag - 1 for points, and 0 for directions.    "
+  "url": "sec-vector-code-start.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Starting a 4D Vector class",
+  "body": " Starting a 4D Vector class   A bare-bones 4D vector class   struct Vec4 { float x, y, z, w; };    "
 },
 {
-  "id": "ch-matrices",
-  "level": "1",
-  "url": "ch-matrices.html",
-  "type": "Chapter",
-  "number": "4",
-  "title": "Matrices",
-  "body": " Matrices   Ok, I admit it - I love quaternions. But all they can do is rotate things. What about moving positions around, making objects bigger or smaller, and even strange things like the perpective projection. Matrices fit the bill nicely here.    "
+  "id": "listing-vector-code-start",
+  "level": "2",
+  "url": "sec-vector-code-start.html#listing-vector-code-start",
+  "type": "Listing",
+  "number": "3.1.1",
+  "title": "",
+  "body": " A bare-bones 4D vector class   struct Vec4 { float x, y, z, w; };   "
 },
 {
-  "id": "ch-dualquats",
+  "id": "sec-matrix-code-start",
   "level": "1",
-  "url": "ch-dualquats.html",
-  "type": "Chapter",
-  "number": "5",
-  "title": "Dual Quaternions",
-  "body": " Dual Quaternions   I find it annoying fiddling around with 16 numbers in a big 4x4 matrix. Isn't there an easier way to rotate and translate things? Yes! Welcome to dual quaternions. Remember how quaternions are just lines that go through the origin. Well what if the line went through something other than the origin? That's all a dual quaternion is, so no need to panic. They are created by two intersecting planes (creating that line I was talking about), and are made up of 8 numbers.    "
+  "url": "sec-matrix-code-start.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Starting a 4D matrix class",
+  "body": " Starting a 4D matrix class   A bare-bones 4x4 matrix class   class Matrix4 { \/\/\/ Let's just make sure that all is clear about how this matrix is layed out. \/\/\/ How a matrix is really layed out is pretty much abitrary but we need to agree \/\/\/ and the world has agreed (except for Microsoft) on the right-hand rule. \/\/\/ First, the 4x4 matrix is really just an array of 16 numbers. \/\/\/ We need to think of the array as a matrix in the following way \/\/\/ 4x4 matrix - COLUMN MAJOR - The OpenGL, science, physics, mathematics and engineering way. \/\/\/ 0 4 8 12 [0][0] [1][0] [2][0] [3][0] \/\/\/ 1 5 9 13 (or) [0][1] [1][1] [2][1] [3][1] \/\/\/ 2 6 10 14 [0][2] [1][2] [2][2] [3][2] \/\/\/ 3 7 11 15 [0][3] [1][3] [2][3] [3][3] private: float m[16]; };    "
+},
+{
+  "id": "listing-matrix-code-start",
+  "level": "2",
+  "url": "sec-matrix-code-start.html#listing-matrix-code-start",
+  "type": "Listing",
+  "number": "4.1.1",
+  "title": "",
+  "body": " A bare-bones 4x4 matrix class   class Matrix4 { \/\/\/ Let's just make sure that all is clear about how this matrix is layed out. \/\/\/ How a matrix is really layed out is pretty much abitrary but we need to agree \/\/\/ and the world has agreed (except for Microsoft) on the right-hand rule. \/\/\/ First, the 4x4 matrix is really just an array of 16 numbers. \/\/\/ We need to think of the array as a matrix in the following way \/\/\/ 4x4 matrix - COLUMN MAJOR - The OpenGL, science, physics, mathematics and engineering way. \/\/\/ 0 4 8 12 [0][0] [1][0] [2][0] [3][0] \/\/\/ 1 5 9 13 (or) [0][1] [1][1] [2][1] [3][1] \/\/\/ 2 6 10 14 [0][2] [1][2] [2][2] [3][2] \/\/\/ 3 7 11 15 [0][3] [1][3] [2][3] [3][3] private: float m[16]; };   "
+},
+{
+  "id": "sec-dualquat-code-start",
+  "level": "1",
+  "url": "sec-dualquat-code-start.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Starting a Dual Quaternion class",
+  "body": " Starting a Dual Quaternion class   A bare-bones Dual Quaternion class   struct DualQuat { float real; float e23; \/\/\/ This is like -i for a regular quaternion. Squares to -1 float e31; \/\/\/ -j float e12; \/\/\/ -k float e01; \/\/\/ Every term with a zero in it squares to zero. It's how translations are handled. float e02; float e03; float e0123; };    "
+},
+{
+  "id": "listing-dualquat-code-start",
+  "level": "2",
+  "url": "sec-dualquat-code-start.html#listing-dualquat-code-start",
+  "type": "Listing",
+  "number": "5.1.1",
+  "title": "",
+  "body": " A bare-bones Dual Quaternion class   struct DualQuat { float real; float e23; \/\/\/ This is like -i for a regular quaternion. Squares to -1 float e31; \/\/\/ -j float e12; \/\/\/ -k float e01; \/\/\/ Every term with a zero in it squares to zero. It's how translations are handled. float e02; float e03; float e0123; };   "
 },
 {
   "id": "backmatter-2",
