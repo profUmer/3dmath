d80801cf845e361a30d690bcca4478f4c30db151
Umer Noor <umer.noor@humber.ca>
2025-02-21 03:17:28 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Plane basics",
-  "body": " Plane basics  It might be useful to start with something that is not a plane, as shown below.   The curvature of space-time around a massive object is not a plane.    Notice how the fabric of space-time in curves around the massive object. That's not a plane. We need a flat sheet, like the image below:  The plane    The whole \"infinitely large and infinitesimally thin\" definition throws off a lot of people. It's hard to imagine something so different to what we see in the real world. But planes are defined by simplest equations. The plane in is described by the equation . That's it! Every point on the plane has an coordinate of zero.  "
+  "body": " Plane basics  It might be useful to start with something that is not a plane, as shown below.   The curvature of space-time around a massive object is not a plane.    Notice how the fabric of space-time in curves around the massive object. That's not a plane. We need a flat sheet, like the image below:   The plane     The whole \"infinitely large and infinitesimally thin\" definition throws off a lot of people. It's hard to imagine something so different to what we see in the real world. But planes are defined by simplest equations. The plane in is described by the equation . That's it! Every point on the plane has an coordinate of zero.  "
 },
 {
   "id": "figure-not-a-plane",
@@ -34,7 +34,7 @@ var ptx_lunr_docs = [
   "type": "Figure",
   "number": "1.1.2",
   "title": "",
-  "body": " The plane    "
+  "body": " The plane     "
 },
 {
   "id": "sec-plane-code-start",
